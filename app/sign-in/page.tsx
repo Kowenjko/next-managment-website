118f938e2fba5eeb45686d59fe1ff91484476af9
Kowenjko <ortoswt@gmail.com>
2026-02-06 14:33:17 +0200

    install Stack Auth

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,6 +1,13 @@
-export default function SignInPage(){
-  return (<div>
+import { SignIn } from "@stackframe/stack";
+import Link from "next/link";
 
-    TEst
-  </div>)
+export default function SignInPage(){
+  return (
+  <div className="min-h-screen flex-center">
+    <div className="max-w-md w-full space-y-8">
+      <SignIn/>
+      <Link href="/" className="underline text-sm"> Go Back Home</Link>
+    </div>
+  </div>
+  )
 }
