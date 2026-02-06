29414ad54c23a0a0858d5d517b5a328542ac9a6e
Kowenjko <ortoswt@gmail.com>
2026-02-06 19:07:14 +0200

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ export default async function Home() {
       <div className="container mx-auto px-4 py-16">
         <div className="text-center">
           <h1 className="text-5xl font-bold text-gray-900 mb-6">Inventory Management</h1>
-          <div>{user ? `Hello, ${user.displayName ?? 'anon'} user_ID: ${user.id}` : 'You are not logged in'}</div>
           <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
             Streamline your inventory tracking with our powerful, easy-to-use management system. Track products, monitor
             stock levels, and gain valuable insights.
